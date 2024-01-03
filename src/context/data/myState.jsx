7145e8ce76745c7d4e0d78ccbba9ed9da00f26ff
Kumar_Aditya(2121841) <18bcs4266@gmail.com>
2024-01-03 18:03:40 +0530

    created nav-bar & hero section

--- src/context/data/myState.jsx
+++ src/context/data/myState.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MyContext from './myContext'
 
-const myState = ({children}) => {
+const myState = (props) => {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+      if (mode === 'light') {
+          setMode('dark');
+          document.body.style.backgroundColor = 'rgb(17, 24, 39)';
+      }
+      else {
+          setMode('light');
+          document.body.style.backgroundColor = 'white';
+
+      }
+  }
   return (
-     <MyContext.Provider>
-          {children}
+     <MyContext.Provider value={{mode,toggleMode}}>
+          {props.children}
      </MyContext.Provider>
   )
 }
 
-export default myState
+export default myState;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from '../../components/layout/Layout'
+import HeroSection from '../../components/heroSection/HeroSection'
 
 const Home = () => {
   return (
     <Layout>
-       Home
+      <HeroSection></HeroSection>
     </Layout>
   )
 }
