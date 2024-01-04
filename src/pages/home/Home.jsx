d4ba2a073cd1a26249e65059a669a885d0d75b29
Kumar_Aditya(2121841) <18bcs4266@gmail.com>
2024-01-04 09:15:56 +0530

    added product cards UI

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Layout from '../../components/layout/Layout'
 import HeroSection from '../../components/heroSection/HeroSection'
+import Filter from '../../components/filter/Filter'
+import ProductCard from '../../components/productCard/ProductCard'
+
+
 
 const Home = () => {
   return (
     <Layout>
       <HeroSection></HeroSection>
+      <Filter></Filter>
+      <ProductCard></ProductCard>
     </Layout>
   )
 }
