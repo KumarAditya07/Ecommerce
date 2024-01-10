60b9f5f61ec1c0fb50b7a2ac0b60c84d7b0d9655
Kumar_Aditya(2121841) <18bcs4266@gmail.com>
2024-01-10 14:00:11 +0530

    added cart feature working on payment gateway

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
+  Navigate,
+  useNavigate,
 } from "react-router-dom";
 
 import Home from './pages/home/Home'
@@ -25,25 +27,74 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
-  <MyState>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/order" element={<Order />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/productinfo/:id" element={<ProductInfo />} />
-        <Route path="/addproduct" element={<AddProduct />} />
-        <Route path="/updateproduct" element={<UpdateProduct/>} />
-        <Route path="/*" element={<NoPage />} />
-      </Routes>
-      <ToastContainer />
-    </Router>
+    <MyState>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/order"
+            element={
+              <ProtectedRoutes>
+                <Order />
+              </ProtectedRoutes>
+            }
+          />
+          <Route path="/cart" element={<Cart />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoutesForAdmin>
+                <Dashboard />
+              </ProtectedRoutesForAdmin>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/productinfo/:id" element={<ProductInfo />} />
+          <Route
+            path="/addproduct"
+            element={
+              <ProtectedRoutesForAdmin>
+                <AddProduct />
+              </ProtectedRoutesForAdmin>
+            }
+          />
+          <Route
+            path="/updateproduct"
+            element={
+              <ProtectedRoutesForAdmin>
+                <UpdateProduct />
+              </ProtectedRoutesForAdmin>
+            }
+          />
+          <Route path="/*" element={<NoPage />} />
+        </Routes>
+        <ToastContainer />
+      </Router>
     </MyState>
-     
   );
 }
 
 export default App
+
+ 
+
+export const ProtectedRoutes = ({ children }) => {
+  if (localStorage.getItem('user')) {
+    return children
+  }
+  else {
+    return <Navigate to='/login' />
+  }
+}
+
+export const ProtectedRoutesForAdmin = ({children}) => {
+  const admin = JSON.parse(localStorage.getItem('user'))
+  console.log(admin.user.email)
+  if (admin.user.email === '18bcs4266@gmail.com') {
+    return children
+  }
+  else {
+    return <Navigate to='/login' />
+  }
+}
--- src/components/navBar/Navbar.jsx
+++ src/components/navBar/Navbar.jsx
@@ -5,6 +5,7 @@ import { BsFillCloudSunFill } from 'react-icons/bs'
 import { FiSun } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 import { RxCross2 } from 'react-icons/rx'
+import { useSelector } from 'react-redux'
 
 const Navbar = () => {
   const context = useContext(myContext)
@@ -12,6 +13,15 @@ const Navbar = () => {
 
   const [open, setOpen] = useState(false)
 
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  
+const logout = () => {
+    localStorage.clear('user');
+    window.location.href = "/";
+}
+
+const cartItems = useSelector((state) => state.cart)
 
   return (
     <div>
@@ -63,17 +73,19 @@ const Navbar = () => {
                     </Link>
                   </div>
 
-                  <div className="flow-root">
+                  {user?.user?.email === '18bcs4266@gmail.com' ? <div className="flow-root">
                     <Link to={'/dashboard'} className="-m-2 block p-2 font-medium text-gray-900" style={{ color: mode === 'dark' ? 'white' : '', }}>
                       admin
                     </Link>
-                  </div>
+                  </div> : ""}
 
-                  <div className="flow-root">
-                    <a className="-m-2 block p-2 font-medium text-gray-900 cursor-pointer" style={{ color: mode === 'dark' ? 'white' : '', }}>
+                  {user ? <div className="flow-root">
+                    <a onClick={logout} className="-m-2 block p-2 font-medium text-gray-900 cursor-pointer" style={{ color: mode === 'dark' ? 'white' : '', }}>
                       Logout
                     </a>
-                  </div>
+                  </div> 
+                : ""}
+                
                   <div className="flow-root">
                     <Link to={'/'} className="-m-2 block p-2 font-medium text-gray-900 cursor-pointer">
                       <img
@@ -141,14 +153,19 @@ const Navbar = () => {
                   <Link to={'/order'} className="text-sm font-medium text-gray-700 " style={{ color: mode === 'dark' ? 'white' : '', }}>
                     Order
                   </Link>
-                  <Link to={'/dashboard'} className="text-sm font-medium text-gray-700 " style={{ color: mode === 'dark' ? 'white' : '', }}>
-                    Admin
-                  </Link>
+                  {user?.user?.email === '18bcs4266@gmail.com' ? <div className="flow-root">
+                    <Link to={'/dashboard'} className="-m-2 block p-2 font-medium text-gray-900" style={{ color: mode === 'dark' ? 'white' : '', }}>
+                      admin
+                    </Link>
+                  </div> : ""}
 
-                  <a className="text-sm font-medium text-gray-700 cursor-pointer  " style={{ color: mode === 'dark' ? 'white' : '', }}>
-                    Logout
-                  </a>
-                </div>
+                  {user ? <div className="flow-root">
+                    <a onClick={logout} className="-m-2 block p-2 font-medium text-gray-900 cursor-pointer" style={{ color: mode === 'dark' ? 'white' : '', }}>
+                      Logout
+                    </a>
+                  </div>  
+                           : ""}
+                 </div>
 
                 <div className="hidden lg:ml-8 lg:flex">
                   <a href="#" className="flex items-center text-gray-700 ">
@@ -188,7 +205,7 @@ const Navbar = () => {
                       <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                     </svg>
 
-                    <span className="ml-2 text-sm font-medium text-gray-700 group-" style={{ color: mode === 'dark' ? 'white' : '', }}>0</span>
+                    <span className="ml-2 text-sm font-bold text-gray-900 group-" style={{ color: mode === 'dark' ? 'white' : '', }}>{cartItems.length}</span>
                     <span className="sr-only">items in cart, view bag</span>
                   </Link>
                 </div>
